59c551e8bd2e74aa5bdd0323b98bd646afcb7307
haupham <duchau1983@yahoo.com>
2019-09-22 11:37:28 -0500

    Add files via upload
    routes for upload photo api

--- server/app.js
+++ server/app.js
@@ -2,6 +2,7 @@ const express = require("express");
 const cors = require("cors");
 
 const mainRouter = require("./routes/main");
+const photoRouter = require("./routes/photo");
 
 const port = process.env.PORT || 3000;
 
@@ -14,6 +15,7 @@ app.use(express.json());
 app.use(cors());
 //todo: add X-Powered By prottection, ETag, etc...
 
-app.use("/fototree-api", mainRouter);
+app.use("/api", mainRouter);
+app.use("/api", photoRouter);
 
 app.listen(port, _ => console.log(`Server is running on port ${port}`));
