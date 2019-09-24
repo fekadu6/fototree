ada8e8a2d1e92d83e0a1e49a4d215a309e857c8b
fekadu6 <fekaduw+1@gmail.com>
2019-09-24 11:41:51 -0500

    after fixing the userState and moving the subscription out of the auth service to components

--- front-end/src/app/account/auth.service.ts
+++ front-end/src/app/account/auth.service.ts
@@ -1,6 +1,6 @@
 import { Injectable } from "@angular/core";
 import { HttpClient } from "@angular/common/http";
-import { Subject } from "rxjs";
+import { BehaviorSubject } from "rxjs";
 import { tap } from "rxjs/operators";
 import { Router } from "@angular/router";
 import { UserState } from "../model/user_state";
@@ -15,54 +15,68 @@ export interface AuthResponseData {
   providedIn: "root"
 })
 export class AuthService {
-  private userStateInfo;
+  private initialState = {
+    userId: null,
+    message: null,
+    token: null
+  };
+  public userStateInfo: UserState = this.initialState;
 
-  private userState$ = new Subject<UserState>();
+  public userState$: BehaviorSubject<UserState> = new BehaviorSubject<
+    UserState
+  >(this.userStateInfo);
 
   baseUrl: string = "http://localhost:3000/fototree-api";
 
   constructor(private http: HttpClient, private router: Router) {}
 
-  getUserState() {
-    return this.userState$.asObservable();
-  }
+  // getUserState() {
+  //   return this.userState$.asObservable();
+  // }
 
   signIn(user) {
     console.log("User info:", user);
 
-    this.http
-      .post<{ message: string; token: string }>(this.baseUrl + "/signin", user)
-      .subscribe(response => {
-        console.log("login response:", response);
-        let newUserStateInfo;
-        if (!response) {
-          newUserStateInfo = null;
-        } else {
-          newUserStateInfo = {
-            userId: user.email,
-            token: response.token,
-            message: response.message
-          };
-        }
+    return this.http.post<{ userId: string; message: string; token: string }>(
+      this.baseUrl + "/signin",
+      user
+    );
+    // .subscribe(response => {
+    //   console.log("login response:", response);
+    //   let newUserStateInfo;
+    //   if (!response) {
+    //     newUserStateInfo = null;
+    //   } else {
+    //     newUserStateInfo = {
+    //       userId: user.email,
+    //       token: response.token,
+    //       message: response.message
+    //     };
+    //   }
 
-        this.userStateInfo = newUserStateInfo;
+    //   this.userStateInfo = newUserStateInfo;
 
-        this.userState$.next(this.userStateInfo);
+    //   this.userState$.next(this.userStateInfo);
 
-        console.log("logged in user", this.userStateInfo);
-      });
+    //   console.log("A", this.userStateInfo);
+    // });
+  }
+
+  getUserState() {
+    return this.userState$;
   }
 
   signUp(user) {
-    this.http.post(this.baseUrl + "/signup", user).subscribe(response => {
-      console.log(response);
-      this.router.navigate(["/signin"]);
-    });
+    return this.http.post(this.baseUrl + "/signup", user);
   }
 
   logOut() {
-    this.userStateInfo = null;
+    this.userStateInfo = this.initialState;
     this.userState$.next(this.userStateInfo);
     this.router.navigate(["/home"]);
+  }
+
+  isAuthenticated() {
+    return !this.userStateInfo.token;
   }
 }
